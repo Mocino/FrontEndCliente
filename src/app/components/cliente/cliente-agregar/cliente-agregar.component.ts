6364091bbc431cba63aae5eba19a5575b426b585
unknown <edrson2@gmail.com>
2024-05-10 09:49:45 -0600

    feat: componentes cliente

---
+++ src/app/components/cliente/cliente-agregar/cliente-agregar.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cliente-agregar',
+  templateUrl: './cliente-agregar.component.html',
+  styleUrls: ['./cliente-agregar.component.css']
+})
+export class ClienteAgregarComponent {
+
+}
