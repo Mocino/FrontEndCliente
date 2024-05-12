fa902be7ba52f98610d996293167b6e8bfb04cb7
unknown <edrson2@gmail.com>
2024-05-12 16:13:52 -0600

    feat: endpoint getCliente

--- src/app/services/Cliente.service.ts
+++ src/app/services/Cliente.service.ts
@@ -1,100 +1,26 @@
 import { Injectable } from '@angular/core';
 import { Cliente} from '../interfaces/Cliente';
 import { Observable, of } from 'rxjs';
+import { enviroment } from 'src/environments/environment';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ClienteService {
 
-  constructor() { }
+  private myAppUrl: string = enviroment.endpoint;
+  private myApiUrl: string = 'api/Clientes/'
+
+  constructor(private http: HttpClient) { }
 
   /**
    * Obtiene la lista de clientes.
    * @returns Un Observable que emite un array de objetos de tipo Cliente.
    */
   getClientes(): Observable<Cliente[]> {
-    const clientesMock: Cliente[] = [
-      {
-        idCliente: 1,
-        nombres: 'Juan',
-        apellidos: 'Pérez',
-        direccion: 'Calle 123, Ciudad',
-        fechaNacimiento: new Date('1990-05-15'),
-        dpi: '1234567890101',
-        nit: 'C123456789',
-        empresa: 'Empresa A',
-        contactos: [],
-        metodosDePago: [
-        ]
-      },
-      {
-        idCliente: 2,
-        nombres: 'María',
-        apellidos: 'López',
-        direccion: 'Avenida 456, Pueblo',
-        fechaNacimiento: new Date('1985-08-20'),
-        dpi: '9876543210101',
-        nit: 'C987654321',
-        empresa: 'Empresa B',
-        contactos: [],
-        metodosDePago: [
-        ]
-      },
-      {
-        idCliente: 3,
-        nombres: 'Pedro',
-        apellidos: 'García',
-        direccion: 'Avenida 789, Pueblo',
-        fechaNacimiento: new Date('1982-03-10'),
-        dpi: '4567890120101',
-        nit: 'C456789012',
-        empresa: 'Empresa C',
-        contactos: [],
-        metodosDePago: [
-        ]
-      },
-      {
-        idCliente: 4,
-        nombres: 'Luisa',
-        apellidos: 'Martínez',
-        direccion: 'Calle 456, Ciudad',
-        fechaNacimiento: new Date('1995-12-28'),
-        dpi: '6543210980101',
-        nit: 'C654321098',
-        empresa: 'Empresa D',
-        contactos: [],
-        metodosDePago: [
-        ]
-      },
-      {
-        idCliente: 5,
-        nombres: 'Carlos',
-        apellidos: 'Rodríguez',
-        direccion: 'Calle 789, Ciudad',
-        fechaNacimiento: new Date('1988-07-15'),
-        dpi: '7890123450101',
-        nit: 'C789012345',
-        empresa: 'Empresa E',
-        contactos: [],
-        metodosDePago: [
-        ]
-      },
-      {
-        idCliente: 6,
-        nombres: 'Pedro',
-        apellidos: 'García',
-        direccion: 'Avenida 789, Pueblo',
-        fechaNacimiento: new Date('1982-03-10'),
-        dpi: '4567890120101',
-        nit: 'C456789012',
-        empresa: 'Empresa C',
-        contactos: [],
-        metodosDePago: [
-        ]
-      }
-    ];
-    return of(clientesMock);
+    return this.http.get<Cliente[]>(`${this.myAppUrl}${this.myApiUrl}`)
+
   }
 
   /**
