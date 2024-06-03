745cb9400b60327b1a82ea85c3e025aae9254af4
unknown <edrson2@gmail.com>
2024-06-03 08:37:21 -0600

    feat: validador email

--- src/app/components/cliente/cliente-separado/contactos/contactos.component.ts
+++ src/app/components/cliente/cliente-separado/contactos/contactos.component.ts
@@ -1,10 +1,12 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, FormArray, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, FormArray, Validators, AbstractControl, ValidationErrors } from '@angular/forms';
 import { MatDialog } from '@angular/material/dialog';
 import { Option } from 'src/app/interfaces/Cliente';
 import { ContactoService } from 'src/app/services/contacto.service';
 import { emailValidator, telefonoValidator } from 'src/app/utils/validador-utils';
 import { ClienteEliminarComponent } from '../../cliente-eliminar/cliente-eliminar.component';
+import { Observable, catchError, debounceTime, map, of, switchMap } from 'rxjs';
+import { ClienteService } from 'src/app/services/Cliente.service';
 
 @Component({
   selector: 'app-contactos',
@@ -19,6 +21,7 @@ export class ContactosComponent implements OnInit {
   constructor(
     private fb: FormBuilder,
     private _contactoService: ContactoService,
+    private _clienteService: ClienteService,
     public _dialog: MatDialog,
   ) { }
 
@@ -27,7 +30,7 @@ export class ContactosComponent implements OnInit {
     if (!this.parentForm.get('contactos')) {
       this.parentForm.addControl('contactos', this.fb.array([]));
     } else if (this.contactos.length === 0) {
-      this.addContacto(); // Añade un contacto vacío si no hay ninguno
+      this.addContacto();
     }
   }
 
@@ -47,10 +50,13 @@ export class ContactosComponent implements OnInit {
       const valorContactoControl = group.get('valorContacto');
       if (tipoContacto === 'email') {
         valorContactoControl?.setValidators([Validators.required, emailValidator()]);
+        valorContactoControl?.setAsyncValidators([this.emailRepeatedValidator()]);
       } else if (tipoContacto === 'teléfono') {
         valorContactoControl?.setValidators([Validators.required, telefonoValidator()]);
+        valorContactoControl?.clearAsyncValidators();
       } else {
         valorContactoControl?.setValidators([Validators.required]);
+        valorContactoControl?.clearAsyncValidators();
       }
       valorContactoControl?.updateValueAndValidity();
     });
@@ -80,5 +86,21 @@ export class ContactosComponent implements OnInit {
       });
     }
   }
+
+  emailRepeatedValidator(): (control: AbstractControl) => Observable<ValidationErrors | null> {
+    return (control: AbstractControl): Observable<ValidationErrors | null> => {
+      if (!control.value) {
+        return of(null);
+      }
+      return this._clienteService.getVerificarEmail(control.value).pipe(
+        map(response => {
+          const res = response as { exists: boolean };
+          return res.exists ? { emailTaken: true } : null;
+        }),
+        catchError(() => of(null))
+      );
+    };
+  }
+
 
 }
